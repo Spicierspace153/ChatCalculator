3c253edfd7714713f22e5ca157ba6ea944509243
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-10-20 15:02:05 -0600

    thanks GTNH!

---
+++ src/main/java/com/Space/ChatCalculator/ClientEventListener.java
@@ -0,0 +1,27 @@
+package com.Space.ChatCalculator;
+
+import appeng.util.NumberUtil;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ChatCalculator.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ClientEventListener {
+
+    @SubscribeEvent
+    public static void onClientChat(ClientChatEvent event) {
+        String message = event.getMessage();
+        if (!message.matches("^= .+")) return;
+        String input = message.substring(2);
+        event.setCanceled(true);
+        double result = NumberUtil.parse(input);
+
+        Minecraft.getInstance().player.displayClientMessage(
+                Component.literal(" " + result).withStyle(ChatFormatting.AQUA),
+                false
+        );
+    }
+}
